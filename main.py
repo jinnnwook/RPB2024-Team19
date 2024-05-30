e73f3664e31ab554fb38853ac9b6503805de8405
강진욱 <165566913+jinnnwook@users.noreply.github.com>
2024-05-31 05:07:55 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -9,29 +9,47 @@ from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import Header
 
 def detect(image):
-    img=image
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    lower_black = np.array([0, 0, 0])
-    upper_black = np.array([180, 255, 50])
-    mask_black = cv2.inRange(hsv_image, lower_black, upper_black)
-    contours, _ = cv2.findContours(mask_black, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    if contours:
-        cnt = max(contours, key=cv2.contourArea)
-        mask_inner = np.zeros_like(mask_black, dtype=np.uint8)
-        cv2.drawContours(mask_inner, [cnt], -1, color=255, thickness=-1)
-        mask_inner = mask_inner > 0
-        mask_red = ((hsv_image[:, :, 0] > 170) | (hsv_image[:, :, 0] < 10)) & (hsv_image[:, :, 1] > 50) & (hsv_image[:, :, 2] > 50)
-        mask_green = (hsv_image[:, :, 0] > 50) & (hsv_image[:, :, 0] < 70) & (hsv_image[:, :, 1] > 50) & (hsv_image[:, :, 2] > 50)
-        mask_blue = (hsv_image[:, :, 0] > 110) & (hsv_image[:, :, 0] < 130) & (hsv_image[:, :, 1] > 50) & (hsv_image[:, :, 2] > 50)
-        red_pixels = np.sum(mask_red & mask_inner)
-        green_pixels = np.sum(mask_green & mask_inner)
-        blue_pixels = np.sum(mask_blue & mask_inner)
-        if red_pixels > green_pixels and red_pixels > blue_pixels:
-            return "R"
-        elif green_pixels > blue_pixels:
-            return "G"
-        else:
-            return "B"
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 1)
+    adaptive_thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 17, 7)
+    contours, _ = cv2.findContours(adaptive_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    red_count=0
+    blue_count=0
+    green_count=0
+    for contour in contours:
+        rect = cv2.minAreaRect(contour)
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        if rect[1][0] > 50 and rect[1][1] > 50:
+            cv2.drawContours(image, [box], 0, (0, 255, 0), 2)
+            width, height = int(rect[1][0]), int(rect[1][1])
+            src_pts = box.astype("float32")
+            dst_pts = np.array([[0, height-1],[0, 0],[width-1, 0],[width-1, height-1]], dtype="float32")
+            M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+            warped = cv2.warpPerspective(image, M, (width, height))
+            hsv = cv2.cvtColor(warped, cv2.COLOR_BGR2HSV)
+            lower_red1 = np.array([0, 51, 51])
+            upper_red1 = np.array([10, 255, 255])
+            lower_red2 = np.array([170, 51, 51])
+            upper_red2 = np.array([180, 255, 255])
+            lower_green = np.array([35, 51, 51])
+            upper_green = np.array([75, 255, 255])
+            lower_blue = np.array([95, 51, 51])
+            upper_blue = np.array([120, 255, 255])
+            red_mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+            red_mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+            red_mask = cv2.bitwise_or(red_mask1, red_mask2)
+            green_mask = cv2.inRange(hsv, lower_green, upper_green)
+            blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
+            red_count += cv2.countNonZero(red_mask)
+            green_count += cv2.countNonZero(green_mask)
+            blue_count += cv2.countNonZero(blue_mask)
+    if red_count > green_count and red_count > blue_count:
+        return 'R'
+    elif green_count > red_count and green_count > blue_count:
+        return 'G'
+    else:
+        return 'B'
 
 class DetermineColor(Node):
     def __init__(self):
